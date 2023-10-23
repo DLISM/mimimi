184157d543abbd2bf72de7d34b9e4338bdca2be1
Javohir <ismoilov92@gmail.com>
2023-10-23 03:49:11 +0300

    fill db

---
+++ src/main/resources/db/migration/V4__Fill_DB.sql
@@ -0,0 +1,6 @@
+insert into cat (name, photo)
+values ('cat 1', '/static/uploads/cat_1.jpg'),
+       ('cat 2', '/static/uploads/cat_2.jpg'),
+       ('cat 3', '/static/uploads/cat_3.jpg'),
+       ('cat 4', '/static/uploads/cat_4.jpg'),
+       ('cat 5', '/static/uploads/cat_5.jpg')
